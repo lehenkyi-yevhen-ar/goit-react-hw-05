f65bb6a48ec496ee6863e7dd2c6293ec706c1ed6
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2024-10-06 01:27:14 +0300

    add lazy+suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,59 @@
 import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home/Home"
-import NotFound from "./pages/NotFound/NotFound"
-import Header from "./components/Header/Header"
-import Movies from "./pages/Movies/Movies"
-import MovieDetails from "./pages/MovieDetails/MovieDetails"
-import Cast from "./components/Cast/Cast"
-import Reviews from "./components/Reviews/Reviews"
+import { lazy, Suspense } from "react"
+import Loader from "./services/loader"
+
+const Home = lazy(() =>
+  import("./pages/Home/Home")
+)
+const Header = lazy(() =>
+  import("./components/Header/Header")
+)
+const NotFound = lazy(() =>
+  import("./pages/NotFound/NotFound")
+)
+const Movies = lazy(() =>
+  import("./pages/Movies/Movies")
+)
+const MovieDetails = lazy(() =>
+  import("./pages/MovieDetails/MovieDetails")
+)
+const Cast = lazy(() =>
+  import("./components/Cast/Cast")
+)
+const Reviews = lazy(() =>
+  import("./components/Reviews/Reviews")
+)
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/movies"
-          element={<Movies />}
-        />
-        <Route
-          path="/movies/:movieId"
-          element={<MovieDetails />}
-        >
-          <Route 
-            path="cast" 
-            element={<Cast />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route
-            path="reviews"
-            element={<Reviews />}
+            path="/movies"
+            element={<Movies />}
           />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route
+            path="/movies/:movieId"
+            element={<MovieDetails />}
+          >
+            <Route
+              path="cast"
+              element={<Cast />}
+            />
+            <Route
+              path="reviews"
+              element={<Reviews />}
+            />
+          </Route>
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+        </Routes>
+      </Suspense>
     </>
   )
 }
