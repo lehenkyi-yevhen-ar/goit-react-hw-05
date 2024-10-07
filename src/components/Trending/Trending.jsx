8515b8d9cf3d04a88fa2f85a2ffab05604603e68
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2024-10-07 22:29:47 +0300

    reduce trending component

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,22 +1,17 @@
 import { useEffect, useState } from "react"
 import { fetchTrendingMovies } from "../../services/api"
-import {
-  Link,
-  useLocation,
-} from "react-router-dom"
 import Loader from "../../services/Loader"
-import s from "./Trending.module.css"
+import MovieList from "../MovieList/MovieList"
 
 const Trending = () => {
-  const [trending, setTrending] = useState([])
+  const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] =
     useState(false)
-  const location = useLocation()
   useEffect(() => {
     const getTrending = async () => {
       setIsLoading(true)
       const data = await fetchTrendingMovies()
-      setTrending(data.results)
+      setMovies(data.results)
       setIsLoading(false)
     }
     getTrending()
@@ -24,32 +19,7 @@ const Trending = () => {
 
   if (isLoading) return <Loader />
 
-  return (
-    <div className={s.container}>
-      <ul className={s.list}>
-        {trending.map((movie) => (
-          <li
-            key={movie.id}
-            className={s.element}
-          >
-            <Link
-              to={`movies/${movie.id}`}
-              state={location}
-            >
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt=""
-                className={s.img}
-              />
-              <p className={s.title}>
-                {movie.title}
-              </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+  return <MovieList movies={movies} />
 }
 
 export default Trending
